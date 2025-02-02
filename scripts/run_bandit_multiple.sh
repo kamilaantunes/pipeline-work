e95505be7199774f0e4c12ee09caf52a24fc7c33
Kamila <kamilaantunes1@gmail.com>
2025-02-02 17:54:24 +0000

    Tests and tests

---
+++ scripts/run_bandit_multiple.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Diretório onde os relatórios serão armazenados
+mkdir -p security_tests
+cd security_tests
+
+# Lista de repositórios a serem testados
+REPOS=(
+    "https://github.com/digininja/DVWA"
+    "https://github.com/juice-shop/juice-shop"
+    "https://github.com/OWASP/SecurityShepherd"
+)
+
+# Clonar e analisar cada repositório
+for repo in "${REPOS[@]}"; do
+    repo_name=$(basename $repo .git)
+    if [ -d "$repo_name" ]; then
+        echo "Repositório $repo_name já existe, pulando clone..."
+    else
+        git clone $repo
+    fi
+    cd $repo_name
+    bandit -r . -f json -o ../bandit_report_${repo_name}.json || echo "Erro ao rodar Bandit em $repo_name"
+    cd ..
+done
+
+echo "Análises concluídas! Relatórios disponíveis em security_tests/"
